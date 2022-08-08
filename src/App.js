5055f6e06973d85e491dd9a33d64640a99580bba
Aitegin <jyldyzbekov06aiti@gmail.com>
2022-08-08 20:37:59 +0600

    feat: added simple text editor without own styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { useState } from "react"
+import parse from "html-react-parser"
 
 function App() {
+  const [text, setText] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className="editor">
+      <h2>Using CKEditor 5 build in React</h2>
+      <CKEditor
+        editor={ClassicEditor}
+        data={ text }
+        onReady={editor => {
+          console.log('Editor is ready to use!', editor);
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          setText(data)
+          console.log({ event, editor, data });
+        }}
+        onBlur={(event, editor) => {
+          console.log('Blur.', editor);
+        }}
+        onFocus={(event, editor) => {
+          console.log('Focus.', editor);
+        }}
+      />
+    </div>
+    <div className='showing_text'>
+      <p>{ parse(text) }</p>
+    </div>
     </div>
   );
 }
